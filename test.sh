91d5e7b2c5b8e927e8bbf1a4311f79fd232d2052
Andr0id88 <1988.hansen@gmail.com>
2020-01-26 21:40:33 +0100

    fixed syntax error

--- test.sh
+++ test.sh
@@ -47,7 +47,7 @@ installationloop() { \
 	"P") pipinstall "$program" "$comment" ;;
 	esac
 	done < /tmp/progs.csv ;}
-}
+
 finalize(){ \
 	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 12 80
 	}
